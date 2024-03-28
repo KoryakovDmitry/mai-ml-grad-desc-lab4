e3c1f78e669ddd7ca2f760ca910b8388eee77e4a
dmitrii_koriakov_mac <dm.korykov@gmail.com>
2024-03-28 19:17:54 +0100

    added task 6;

--- utils/regression_helpers.py
+++ utils/regression_helpers.py
@@ -1,4 +1,5 @@
 import copy
+import time
 
 from modules.linear_regression import LinearRegression
 
@@ -83,6 +84,43 @@ def fit_and_evaluate_task52(cfg_name, lambda_value, descent_config, tolerance, m
 
     result = {
         'lambda': lambda_value,
+        'train_error': train_error,
+        'train_r2': train_r2,
+        'val_error': val_error,
+        'val_r2': val_r2,
+        'test_error': test_error,
+        'test_r2': test_r2,
+        'last_iteration_step': regression.last_iter,
+        'loss_history': regression.loss_history
+    }
+
+    return cfg_name, result
+
+
+def fit_and_evaluate_task6(cfg_name, batch_size, descent_config, tolerance, max_iter, X_train, y_train, X_val, y_val,
+                           X_test, y_test):
+    descent_config = copy.deepcopy(descent_config)
+    descent_config['kwargs']['batch_size'] = batch_size
+
+    regression = LinearRegression(
+        descent_config=descent_config,
+        tolerance=tolerance,
+        max_iter=max_iter
+    )
+    start_time = time.time()
+    regression.fit(X_train, y_train)
+    training_time = time.time() - start_time
+
+    train_error = regression.calc_loss(X_train, y_train)
+    train_r2 = r2_score(y_true=y_train, y_pred=regression.predict(X_train))
+    val_error = regression.calc_loss(X_val, y_val)
+    val_r2 = r2_score(y_true=y_val, y_pred=regression.predict(X_val))
+    test_error = regression.calc_loss(X_test, y_test)
+    test_r2 = r2_score(y_true=y_test, y_pred=regression.predict(X_test))
+
+    result = {
+        'batch_size': int(batch_size),
+        'training_time': training_time,
         'train_error': train_error,
         'train_r2': train_r2,
         'val_error': val_error,
